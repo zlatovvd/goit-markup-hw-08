2487aeadf03ce47581754b0e523e3da1d78c863a
zlatovvd <31865609+zlatovvd@users.noreply.github.com>
2023-01-10 23:33:38 +0200

    upd 10.01.23

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector("[data-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleModal);
+    refs.closeMenuBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      console.log("click");
+      refs.menu.classList.toggle("is-open");
+    }
+  })();
